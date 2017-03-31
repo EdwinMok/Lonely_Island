12c7d18df082ad9ff80685e4f109a39390e503b5
EdwinMok <emok@stuy.edu>
2017-03-31 11:36:54 -0400

    Added QQKachoo.java

---
+++ QQKachoo.java
@@ -0,0 +1,47 @@
+public class QQKachoo<T> implements Deque<T>{
+
+    //Default constructor
+    public QQKachoo(){
+	
+    }
+
+    //
+    public void addFirst(T x){
+
+    }
+
+    //
+    public void addLast(T x){
+
+    }
+
+    //
+    public T removeFirst(){
+
+    }
+
+    //
+    public T removeLast(){
+
+    }
+
+    //
+    public T peekFirst(){
+
+    }
+
+    //
+    public T peekLast(){
+
+    }
+
+    //
+    public boolean isEmpty(){
+	
+    }
+
+    //For testing purposes
+    public static void main(String[] args){
+
+    }
+}
